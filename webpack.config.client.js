3a4a260de7c5bc1040b7ca2df861b5b5557b3a1d
Trashboat-99 <s.honore99@gmail.com>
2018-06-03 16:20:28 -0400

    updated package.json

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -24,6 +24,10 @@ const config = {
                 use: [
                     'babel-loader'
                 ]
+            },
+            {
+                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+                use: 'file-loader'
             }
         ]
     },  plugins: [
